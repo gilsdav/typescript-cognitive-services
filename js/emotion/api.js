398fc52887dbac46008608c714a32a8b8d849681
David Gilson <davgilson@live.fr>
2017-03-15 23:17:35 +0100

    config build library

---
+++ js/emotion/api.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var EmotionApi = (function () {
+    function EmotionApi() {
+    }
+    return EmotionApi;
+}());
+exports.EmotionApi = EmotionApi;
---
+++ ts/emotion/api.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var EmotionApi = (function () {
+    function EmotionApi() {
+    }
+    return EmotionApi;
+}());
+exports.EmotionApi = EmotionApi;
